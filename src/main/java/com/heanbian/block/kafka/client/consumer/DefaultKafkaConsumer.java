7f2d1b141718ac132952a478ae4d175d0581d044
马洪 <Administrator@USER-20180910GE>
2019-08-06 10:26:14 +0800

    1

--- src/main/java/com/heanbian/block/kafka/client/consumer/DefaultKafkaConsumer.java
+++ src/main/java/com/heanbian/block/kafka/client/consumer/DefaultKafkaConsumer.java
@@ -94,11 +94,12 @@ public class DefaultKafkaConsumer implements InitializingBean {
 		public void run() {
 			try {
 				for (ConsumerRecord<String, byte[]> record : records) {
+					if (valueClass == null) {
+						valueClass = method.getParameterTypes()[0];
+					}
+					String metadata = new String(record.value(), Charset.defaultCharset());
 					method.setAccessible(true);
-					//Class<?> valueClass = method.getParameterTypes()[0];
-					method.invoke(bean, (valueClass == String.class)
-							? new String(record.value(), Charset.defaultCharset())
-							: JSON.parseObject(new String(record.value(), Charset.defaultCharset()), valueClass));
+					method.invoke(bean, (valueClass == String.class) ? metadata : JSON.parseObject(metadata, valueClass));
 				}
 			} catch (Exception e) {
 				if (LOGGER.isErrorEnabled()) {
